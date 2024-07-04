92b762add17d742eac24b45fc10983c5cd9f7337
ahn-sj <jaesa5221@gmail.com>
2024-07-04 21:03:08 +0900

    Initial Project

---
+++ src/main/java/com/example/parser/ParseCsvAndXlsxApplication.java
@@ -0,0 +1,13 @@
+package com.example.parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParseCsvAndXlsxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParseCsvAndXlsxApplication.class, args);
+    }
+
+}
